b56295a1713e46cc9ad642d99ea5533ff6f8a07f
"Lester Caine ext:(%22) <lester@lsces.co.uk>
2010-09-09 10:03:19 +0100

    tidy shell scripts

--- themes/hgsub_init.sh
+++ themes/hgsub_init.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 # Uncomment additional themes that you want to add to the repository
 # Additional themes can be added later by simply cloning them to the config/themes folder
-hg clone git+ssh://git@github.com:bitweaver/theme_basic.git basic 
-hg clone git+ssh://git@github.com:bitweaver/theme_firebird.git firebird 
-#hg clone git+ssh://git@github.com:bitweaver/theme_pillbox.git pillbox 
-#hg clone git+ssh://git@github.com:bitweaver/theme_blueprint.git blueprint 
+hg clone git+ssh://git@github.com:bitweaver/theme_basic.git basic;
+hg clone git+ssh://git@github.com:bitweaver/theme_firebird.git firebird;
+# hg clone git+ssh://git@github.com:bitweaver/theme_pillbox.git pillbox;
+# hg clone git+ssh://git@github.com:bitweaver/theme_blueprint.git blueprint;
 
