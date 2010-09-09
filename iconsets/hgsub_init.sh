e57bf52396ce1c9006ce67de8fee97853d57772e
Lester Caine <lester@srv1.lsces.co.uk>
2010-09-09 08:27:47 +0100

    add blank iconsets and themes folder with configuration file

---
+++ iconsets/hgsub_init.sh
@@ -0,0 +1,6 @@
+# Uncomment additional iconsets that you want to add to the repository
+# Additional iconsets can be added later by simply cloning them to the config/iconsets folder
+hg clone git+ssh://git@github.com:bitweaver/iconset_tango.git tango 
+hg clone git+ssh://git@github.com:bitweaver/iconset_gnome.git gnome 
+#hg clone git+ssh://git@github.com:bitweaver/iconset_highcontrast.git highcontrast 
+
